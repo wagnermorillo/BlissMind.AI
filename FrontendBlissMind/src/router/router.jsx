2641bc61101e7b398c42d5401ab7a2672ea9b171
Susan Paulino <1105385@est.intec.edu.do>
2025-04-15 04:54:05 -0400

    add dashboard patient to the router

--- FrontendBlissMind/src/router/router.jsx
+++ FrontendBlissMind/src/router/router.jsx
@@ -1,34 +1,41 @@
-import {lazy, React, Suspense} from "react";
-import { Routes, Route} from "react-router-dom";
-import ProtectedRouter from './ProtectedRouter.jsx'
+import { lazy, React, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRouter from "./ProtectedRouter.jsx";
 const Login = lazy(() => import("../pages/Login.jsx"));
 const Register = lazy(() => import("../pages/Register.jsx"));
-const Welcomescreen = lazy(() => import("../components/Questions/Welcomescreen.jsx"));
+const Welcomescreen = lazy(() =>
+  import("../components/Questions/Welcomescreen.jsx")
+);
 const Welcpsyco = lazy(() => import("../components/Questions/Welcpsyco.jsx"));
-const Questionnaire = lazy(() => import("../components/Questions/Questionnaire.jsx"));
+const Questionnaire = lazy(() =>
+  import("../components/Questions/Questionnaire.jsx")
+);
 const Upload = lazy(() => import("../components/Questions/Upload.jsx"));
 const Home = lazy(() => import("../pages/Home"));
 const LoadingScreen = lazy(() => import("../components/Loading/loading.jsx"));
 const DashBoard = lazy(() => import("../pages/dashboard/dashboard.jsx"));
 const ChatAi = lazy(() => import("../pages/chat-ai/ChatAi.jsx"));
+const DashboardPatient = lazy(() => import("../pages/dashboard/DashboardPatient.jsx"));
 
 function AppRouter() {
   return (
-        <Suspense fallback={<LoadingScreen />}>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/home" element={<Home />}/>
-          <Route path="/Login" element={<Login />}/>
-          <Route path="/Register" element={<Register />}/>
-          <Route path="/" element={<ProtectedRouter />}>
+    <Suspense fallback={<LoadingScreen />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/" element={<ProtectedRouter />}>
           <Route path="/Dashboard" element={<DashBoard />} />
-          </Route>          <Route path="/Welcomescreen" element={<Welcomescreen />}/>
-          <Route path="/Questionnaire" element={<Questionnaire />}/>
-          <Route path="/Welcpsyco" element={<Welcpsyco />}/>
-          <Route path="/Upload" element={<Upload />}/>
-          <Route path="/chat-ai" element={<ChatAi />} />
-        </Routes>
-        </Suspense>
+        </Route>
+        <Route path="/Welcomescreen" element={<Welcomescreen />} />
+        <Route path="/Questionnaire" element={<Questionnaire />} />
+        <Route path="/Welcpsyco" element={<Welcpsyco />} />
+        <Route path="/Upload" element={<Upload />} />
+        <Route path="/dashboard-patient" element={<DashboardPatient />} />
+        <Route path="/chat-ai" element={<ChatAi />} />
+      </Routes>
+    </Suspense>
   );
 }
 
